b07782add16d57617f22665b0a0ef1a00a5b6a57
TamSen7525 <kadyev_edem@mail.ru>
2025-02-24 08:59:11 +0300

    task_8

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,18 @@
   <!-- Task 1 -->
   <div>
     <h1>Task 1</h1>
-    <p>{{ arr[0] }}</p>
-    <p>{{ arr[1] }}</p>
-    <p>{{ arr[2] }}</p>
-  </div>
-  
-  <!-- Task 2 -->
-  <div>
-    <h1>Task 2</h1>
-    <p>{{ arr1[0] + arr1[1] + arr1[2] }}</p>
+    <p>{{ obj.x + obj.y + obj.z }}</p>
+
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      arr: ['x', 'y', 'z'],
-      arr1: [1, 2, 3],
-    }
-  }
+	return {
+		obj: {x: 1, y: 2, z: 3},
+	}
+}
 }
 </script>
 
