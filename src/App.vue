033f36e58434578b85c5a0fccebb49ee9bab1306
TamSen7525 <kadyev_edem@mail.ru>
2025-02-24 09:03:02 +0300

    task_9

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,18 @@
   <!-- Task 1 -->
   <div>
     <h1>Task 1</h1>
-    <p>{{ obj.x + obj.y + obj.z }}</p>
+    <button @click="showDate">Show Date</button>
 
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-	return {
-		obj: {x: 1, y: 2, z: 3},
-	}
-}
+  methods: {
+    showDate() {
+      alert(new Date());
+    }
+  } 
 }
 </script>
 
