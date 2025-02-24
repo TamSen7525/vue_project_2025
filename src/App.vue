187bf59295f4f7658497dfd12c89f5efb629b24f
TamSen7525 <kadyev_edem@mail.ru>
2025-02-24 08:46:54 +0300

    task_6

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-<a href="page.html">page</a>
+  <!-- Task 1 -->
+{{ num * num }}
+  <!-- Task 2 -->
+{{ num1 + num2 + num3 }}
 </template>
 
 <script>
 export default {
   data() {
 	return {
-		text: 'page',
-		href: 'page.html',
+		num: 5,
+    num1: 1,
+		num2: 2,
+		num3: 3,
 	}
 }
   }
