f8a3efe14b6bc176c4532ff026499cdae7f85328
TamSen7525 <kadyev_edem@mail.ru>
2025-02-24 08:56:34 +0300

    task_7

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <!-- Task 1 -->
-{{ num * num }}
+  <div>
+    <h1>Task 1</h1>
+    <p>{{ arr[0] }}</p>
+    <p>{{ arr[1] }}</p>
+    <p>{{ arr[2] }}</p>
+  </div>
+  
   <!-- Task 2 -->
-{{ num1 + num2 + num3 }}
+  <div>
+    <h1>Task 2</h1>
+    <p>{{ arr1[0] + arr1[1] + arr1[2] }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-	return {
-		num: 5,
-    num1: 1,
-		num2: 2,
-		num3: 3,
-	}
-}
+    return {
+      arr: ['x', 'y', 'z'],
+      arr1: [1, 2, 3],
+    }
   }
+}
 </script>
+
+<style scoped>
+
+div {
+  margin-bottom: 20px;
+}
+
+h1 {
+  margin: 0 0 10px 0;
+}
+
+p {
+  margin: 5px 0;
+}
+</style>
