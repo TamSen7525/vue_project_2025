8aec7812bc9032b4b3488b31f0745f985d342a61
TamSen7525 <kadyev_edem@mail.ru>
2025-02-27 08:27:25 +0300

    task_49

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <template>
   <div>
     <h1>Task 1</h1>
-    <div>
-      <input name="language" type="radio" v-model="choice" value="English"> English
-      <input name="language" type="radio" v-model="choice" value="Spanish"> Spanish
-      <input name="language" type="radio" v-model="choice" value="French"> French
-    </div>
-    <p>You choosed: {{ choice }}</p>
-
-    <h1>Task 2</h1>
-    <div>
-      <input name="language" type="radio" v-model="choice" value="English"> English
-      <input name="language" type="radio" v-model="choice" value="Spanish"> Spanish
-      <input name="language" type="radio" v-model="choice" value="French"> French
-    </div>
-    <p v-if="choice === 'English'">Hello!</p>
-    <p v-if="choice === 'Spanish'">¡Hola!</p>
-    <p v-if="choice === 'French'">Bonjour!</p>
+    <select v-model="selectedCity">
+      <option>New York</option>
+      <option>Los Angeles</option>
+      <option>Chicago</option>
+    </select>
+    <p>{{ selectedCity }}</p>
   </div>
 </template>
 
@@ -24,8 +14,8 @@
 export default {
   data() {
     return {
-      choice: ''
+      selectedCity: 'New York',
     };
-  }
+  },
 };
 </script>
