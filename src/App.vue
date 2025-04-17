23a7df96fb2a814448907fc0aa3a572a34650f95
TamSen7525 <kadyev_edem@mail.ru>
2025-04-17 09:14:07 +0300

    task_54

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
   <div>
     <h1>Task 1</h1>
-    <input v-model="newItem" />
-    <button @click="addItem">Add</button>
     <ul>
-      <li v-for="(item, index) in items" :key="index">{{ item }}</li>
-    </ul>
-
-    <h1>Task 2</h1>
-    <input v-model="newItem2" />
-    <button @click="addItemToStart">Add</button>
-    <ul>
-      <li v-for="(item, index) in items2" :key="index">{{ item }}</li>
+      <li v-for="(item, index) in items" :key="index" @click="removeItem(index)">
+        {{ item }}
+      </li>
     </ul>
   </div>
 </template>
@@ -20,24 +13,12 @@
 export default {
   data() {
     return {
-      newItem: "",
-      items: ["a", "b", "c", "d", "e"],
-      newItem2: "",
-      items2: ["a", "b", "c", "d", "e"]
+      items: ["a", "b", "c", "d", "e"]
     };
   },
   methods: {
-    addItem() {
-      if (this.newItem.trim()) {
-        this.items.push(this.newItem);
-        this.newItem = "";
-      }
-    },
-    addItemToStart() {
-      if (this.newItem2.trim()) {
-        this.items2.unshift(this.newItem2);
-        this.newItem2 = "";
-      }
+    removeItem(index) {
+      this.items.splice(index, 1);
     }
   }
 };
