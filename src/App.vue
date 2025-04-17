26cf5a15d2a2054f39d0eaab93851b0a8879a39f
TamSen7525 <kadyev_edem@mail.ru>
2025-04-17 09:14:38 +0300

    task_55

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div>
     <h1>Task 1</h1>
-    <ul>
-      <li v-for="(item, index) in items" :key="index" @click="removeItem(index)">
-        {{ item }}
-      </li>
-    </ul>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Salary</th>
+          <th>Age</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td>{{ user.name }}</td>
+          <td>{{ user.salary }}</td>
+          <td>{{ user.age }}</td>
+          <td><a href="#" @click="removeItem(user.id)">Delete</a></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -13,12 +26,31 @@
 export default {
   data() {
     return {
-      items: ["a", "b", "c", "d", "e"]
+      users: [
+        {
+          id: 1,
+          name: "name1",
+          salary: 100,
+          age: 30
+        },
+        {
+          id: 2,
+          name: "name2",
+          salary: 200,
+          age: 40
+        },
+        {
+          id: 3,
+          name: "name3",
+          salary: 300,
+          age: 50
+        }
+      ]
     };
   },
   methods: {
-    removeItem(index) {
-      this.items.splice(index, 1);
+    removeItem(id) {
+      this.users = this.users.filter(user => user.id !== id);
     }
   }
 };
