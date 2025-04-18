4cfdb2f6c13f2d0e055a9fdc721a467ed098a0d5
TamSen7525 <kadyev_edem@mail.ru>
2025-04-18 15:47:06 +0300

    task_58

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,16 @@
+<!-- App.vue (main component) -->
 <template>
   <div>
-    <h1>Task 1</h1>
-    <table border="1">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Surname</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in users" :key="user.id">
-          <td>
-            <template v-if="!user.isEdit">{{ user.name }}</template>
-            <template v-else><input v-model="user.name" /></template>
-          </td>
-          <td>
-            <template v-if="!user.isEdit">{{ user.surn }}</template>
-            <template v-else><input v-model="user.surn" /></template>
-          </td>
-          <td>
-            <template v-if="!user.isEdit">
-              <button @click="edit(user)">Edit</button>
-            </template>
-            <template v-else>
-              <button @click="save(user)">Save</button>
-            </template>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <Employee />
   </div>
 </template>
 
 <script>
+import Employee from './components/Employee.vue'
+
 export default {
-  data() {
-    return {
-      users: [
-        {
-          id: 1,
-          name: "name1",
-          surn: "surn1",
-          isEdit: false
-        },
-        {
-          id: 2,
-          name: "name2",
-          surn: "surn2",
-          isEdit: false
-        },
-        {
-          id: 3,
-          name: "name3",
-          surn: "surn3",
-          isEdit: false
-        }
-      ]
-    };
-  },
-  methods: {
-    edit(user) {
-      user.isEdit = true;
-    },
-    save(user) {
-      user.isEdit = false;
-    }
+  components: {
+    Employee
   }
 };
 </script>
---
+++ src/components/Employee.vue
@@ -0,0 +1,18 @@
+<!-- Employee.vue -->
+<template>
+    <div>
+      <p>{{ name }} - {{ position }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: "John Doe",
+        position: "Software Developer"
+      };
+    }
+  };
+  </script>
+  
