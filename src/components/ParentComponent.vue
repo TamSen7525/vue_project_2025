48b8007b8a4776299276c7d2de3b9ff94efbff72
TamSen7525 <kadyev_edem@mail.ru>
2025-04-18 15:58:43 +0300

    task_70

--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -1,43 +1,29 @@
 <template>
-    <div>
-      <Employee
-        v-for="user in users"
-        :key="user.id"
-        :id="user.id"
-        :name="user.name"
-        :surn="user.surn"
-        @change="change"
-      />
-    </div>
+    <UserForm @add="add" />
   </template>
   
   <script>
-  import Employee from './components/Employee.vue';
+  import UserForm from './UserForm.vue'
   
   export default {
+    components: {
+      UserForm
+    },
     data() {
       return {
-        users: [
-          { id: 1, name: 'name1', surn: 'surn1' },
-          { id: 2, name: 'name2', surn: 'surn2' },
-          { id: 3, name: 'name3', surn: 'surn3' },
-        ],
-      };
+        users: []
+      }
     },
     methods: {
-      change(id, name, surn) {
-        this.users = this.users.map((user) => {
-          if (user.id === id) {
-            user.name = name;
-            user.surn = surn;
-          }
-          return user;
+      add(name, surn) {
+        let id = this.users.length + 1;
+        this.users.push({
+          id,
+          name,
+          surn
         });
-      },
-    },
-    components: {
-      Employee,
-    },
-  };
+      }
+    }
+  }
   </script>
   
---
+++ src/components/UserForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <input v-model="newName">
+    <input v-model="newSurn">
+    <button @click="save">save</button>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newName: '',
+        newSurn: '',
+      }
+    },
+    methods: {
+      save() {
+        this.$emit('add', this.newName, this.newSurn);
+      }
+    }
+  }
+  </script>
+  
