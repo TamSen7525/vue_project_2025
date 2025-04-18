eabfa4901d72f7622d190c49183fec34ba482048
TamSen7525 <kadyev_edem@mail.ru>
2025-04-18 15:52:16 +0300

    task_63

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <Employee :name="name" :salary="salary" :age="age" />
+    <Employee
+      v-for="user in users"
+      :key="user.id"
+      :name="user.name"
+      :salary="user.salary"
+      :age="user.age"
+    />
   </div>
 </template>
 
@@ -10,9 +16,26 @@ import Employee from './components/Employee.vue'
 export default {
   data() {
     return {
-      name: 'John',
-      salary: 50000,
-      age: 30
+      users: [
+        {
+          id: 1,
+          name: 'name1',
+          salary: 100,
+          age: 30,
+        },
+        {
+          id: 2,
+          name: 'name2',
+          salary: 200,
+          age: 40,
+        },
+        {
+          id: 3,
+          name: 'name3',
+          salary: 300,
+          age: 50,
+        },
+      ],
     };
   },
   components: {
